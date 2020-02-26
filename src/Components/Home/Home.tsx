8f65eada9a5bf6f64f06086132f9c18a9fad1e96
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-27 02:33:14 +0530

    Home
    Initialized and connected
    -home page
    -style home page

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  ToastAndroid,
+  Button
+} from "react-native";
+import styles from "./HomeStyles"
