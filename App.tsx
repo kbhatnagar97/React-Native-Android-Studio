490175b4eab2a6d7ee82286f58e1d58cb1541e6f
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-26 15:43:43 +0530

    Component creation
    Used components to print hello world

--- App.tsx
+++ App.tsx
@@ -1,35 +1,10 @@
-
-
 import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Login } from "./src/Components/login/Login"
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.hello}>
-        Hello world!
-      </Text>
-    </View>
+    <Login />
 );
-}
-
-const styles = StyleSheet.create({
-  hello : {
-    display: "flex" ,
-    color: "red",
-    marginTop: 100,
-    justifyContent: "center",
-    textAlign: "center"
-  }
-
-})
-  
+} 
 
 export default App;
